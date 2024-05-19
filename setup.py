b993f3edaa3e67a3c8a722b13a0d1dc6b3b1e723
Ricardo Steijn <ricardo.steijn97@gmail.com>
2024-05-19 20:37:59 +0200

    chore: release version 2.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='crownstone-sse',
-    version="2.0.4",
+    version="2.0.5",
     url='https://github.com/Crownstone-Community/crownstone-lib-python-sse',
     author='Crownstone B.V.',
     long_description=long_description,
